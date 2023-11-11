9a7392ed09e614467a87c91537630d4f4c7aa545
Iacob Alexandru <iacobalex11@yahoo.com>
2023-11-11 16:41:29 +0200

    first commit

---
+++ src/store/modules/coaches/actions.js
@@ -0,0 +1,60 @@
+export default {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
+        const coachData = {
+            firstName: data.first,
+            lastName: data.last,
+            description: data.desc,
+            hourlyRate: data.rate,
+            areas: data.areas
+        };
+
+        const response = await fetch(`https://console.firebase.google.com/project/find-a-coach-32c42/database/find-a-coach-32c42-default-rtdb/data/~2F/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            return;
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
+    },
+
+    async loadCoaches(context, payload) {
+
+        if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+            return;
+        }
+
+        const response = await fetch(`https://console.firebase.google.com/project/find-a-coach-32c42/database/find-a-coach-32c42-default-rtdb/data/~2F/coaches.json`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch!');
+            throw error;
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
+    }
+};
